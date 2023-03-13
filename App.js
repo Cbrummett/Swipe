d028158c8c4fbeb0bd3941719c801a49903f2580
Collyn Brummett <cb717599@southeast.edu>
2023-03-13 16:20:59 -0500

    initial commit

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Profile">
+        <Drawer.Screen name="Profile" component={Profile} />
+        <Drawer.Screen name="CreateAPost" component={CreateAPost} />
+      </Drawer.Navigator>
+    </NavigationContainer>
+    
   );
+}
+function Profile({ navigation }) {
+  return(
+    <View>
+      <Text>This is the profile page</Text>
+    </View>
+  )
+}
+
+function CreateAPost({ navigation }) {
+  return(
+    <View>
+      <Text>This is the Create a Post page</Text>
+    </View>
+  )
 }
 
 const styles = StyleSheet.create({
